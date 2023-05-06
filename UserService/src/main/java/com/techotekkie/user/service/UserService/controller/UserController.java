c4debc3b801edefa1ff55ac01d4b953510d92f3b
chandan90kr <chandan.kr.rimt@gmail.com>
2023-05-07 01:32:16 +0530

    Implementation of Reselience4j for failure of Dependent API

--- UserService/src/main/java/com/techotekkie/user/service/UserService/controller/UserController.java
+++ UserService/src/main/java/com/techotekkie/user/service/UserService/controller/UserController.java
@@ -1,12 +1,18 @@
 package com.techotekkie.user.service.UserService.controller;
 import com.techotekkie.user.service.UserService.entities.User;
 import com.techotekkie.user.service.UserService.services.UserServices;
+import com.techotekkie.user.service.UserService.services.impl.UserServiceImpl;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
+
+
 //TODO: This class is used handle the controller class where we write our logic to implement or start
 @RestController
 @RequestMapping("/users")
@@ -16,6 +22,8 @@ public class UserController {
 
     @Autowired
     private UserServices userServices;
+
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user)
     {
@@ -24,10 +32,24 @@ public class UserController {
     }
 
     @GetMapping("/{userId}")
+    @CircuitBreaker(name = "ratingHotelBreaker",fallbackMethod = "ratingHotelFallback")
+
     public  ResponseEntity<User> getSingleUser(@PathVariable String userId){
 
         User user= userServices.getUser(userId);
         return ResponseEntity.ok(user);
+
+    }
+    //creating fallback method for circuitbreaker
+    public ResponseEntity<User> ratingHotelFallback(String userId,Exception ex){
+        logger.info("Fallback is executed because service is dow : ",ex.getMessage());
+        User user = User.builder()
+                    .email("dummy@gmail.com")
+                    .name("Dummy")
+                    .about("This user is created dummy because some service is down")
+                    .userId("11231")
+                    .build();
+        return new ResponseEntity<>(user, HttpStatus.OK);
 
     }
 
